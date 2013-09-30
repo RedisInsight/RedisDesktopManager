324b3d297b86197447fb3ad206cbec5052d68f4a
Igor Malinovskiy <u.glide@gmail.com>
2013-09-30 15:54:09 +0300

    Replace ubuntu-sdk by building qt from source

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
 sudo apt-get update 
-sudo apt-get install python-software-properties -y
-sudo apt-add-repository ppa:ubuntu-sdk-team/ppa -y
-sudo apt-get update
-sudo apt-get install ubuntu-sdk -y
-sudo apt-get install libssl0.9.8 -y
-sudo apt-get install git -y
+sudo apt-get install git python perl libssl0.9.8 -y
+sudo apt-get install "^libxcb.*" libx11-xcb-dev libglu1-mesa-dev libxrender-dev libatspi libdbus-1-dev -y 
 
 #setup build env
 sudo apt-get install build-essential devscripts ubuntu-dev-tools debhelper \
@@ -17,3 +13,23 @@ cd /home/vagrant
 echo 'COMPONENTS="main restricted universe multiverse"' > ~/.pbuilderrc
 
 sudo pbuilder create
+
+#get and build qt
+
+QTBUILD_DIR=/home/vagrant/qt
+INSTALL_ARC=$QTBUILD_DIR/qt-everywhere-opensource-src-5.1.1.tar.gz
+
+echo $INSTALL_ARC
+
+if [ ! -f $INSTALL_ARC ]
+then
+	wget http://download.qt-project.org/official_releases/qt/5.1/5.1.1/single/qt-everywhere-opensource-src-5.1.1.tar.gz	
+	tar -xvf $INSTALL_ARC
+fi
+
+cd $QTBUILD_DIR/qt-everywhere-opensource-src-5.1.1
+
+sudo ./configure -opensource -qt-libpng -qt-libjpeg -qt-xcb -qt-xkbcommon -no-kms -no-opengl -dbus-linked -nomake examples -confirm-license
+
+sudo make
+sudo make install
