4f42e4ae5440b16b63660fbaabc9d99ce628bef7
Igor Malinovskiy <u.glide@gmail.com>
2013-09-30 16:43:48 +0300

    Updated install script

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,25 +1,29 @@
 #!/bin/sh
 
-sudo apt-get update 
-sudo apt-get install git python perl libssl0.9.8 -y
-sudo apt-get install "^libxcb.*" libx11-xcb-dev libglu1-mesa-dev libxrender-dev libatspi libdbus-1-dev -y 
+#sudo apt-get update 
+#sudo apt-get install git python perl libssl0.9.8 -y
+#sudo apt-get install "^libxcb.*" libx11-xcb-dev libglu1-mesa-dev libxrender-dev -y 
+sudo apt-get install libatspi-dev -y
+sudo apt-get install libdbus-1-dev -y
 
 #setup build env
-sudo apt-get install build-essential devscripts ubuntu-dev-tools debhelper \
-        dh-make diff patch gnupg fakeroot lintian pbuilder -y
+#sudo apt-get install build-essential devscripts ubuntu-dev-tools debhelper \
+#        dh-make diff patch gnupg fakeroot lintian pbuilder -y
 
 cd /home/vagrant
 
 echo 'COMPONENTS="main restricted universe multiverse"' > ~/.pbuilderrc
 
-sudo pbuilder create
+#sudo pbuilder create
 
 #get and build qt
 
-QTBUILD_DIR=/home/vagrant/qt
+QTBUILD_DIR=`pwd`/qt
 INSTALL_ARC=$QTBUILD_DIR/qt-everywhere-opensource-src-5.1.1.tar.gz
 
+mkdir $QTBUILD_DIR
 echo $INSTALL_ARC
+cd $QTBUILD_DIR
 
 if [ ! -f $INSTALL_ARC ]
 then
@@ -27,7 +31,7 @@ then
 	tar -xvf $INSTALL_ARC
 fi
 
-cd $QTBUILD_DIR/qt-everywhere-opensource-src-5.1.1
+cd ./qt-everywhere-opensource-src-5.1.1
 
 sudo ./configure -opensource -qt-libpng -qt-libjpeg -qt-xcb -qt-xkbcommon -no-kms -no-opengl -dbus-linked -nomake examples -confirm-license
 
