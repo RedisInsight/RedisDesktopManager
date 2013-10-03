4adb6e50f01bdf735c6ff8c348688dcb7b4f0fa7
Igor Malinovskiy <u.glide@gmail.com>
2013-10-03 15:05:04 +0300

    Up Version to 0.4.0

--- redis-desktop-manager/source/main.cpp
+++ redis-desktop-manager/source/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 	QApplication a(argc, argv);
 
 	QApplication::setApplicationName("Redis Desktop Manager");
-	QApplication::setApplicationVersion("0.3.1");	
+	QApplication::setApplicationVersion("0.4.0");	
 	QApplication::setOrganizationDomain("redisdesktop.com");
 
 	Main w;
