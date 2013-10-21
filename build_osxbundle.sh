0788295555dcb7a71463ddcec8155591d688ae1c
Igor Malinovskiy <u.glide@gmail.com>
2013-10-21 18:03:17 +0400

    Improved osx bundle

--- build_osxbundle.sh
+++ build_osxbundle.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-BUILD_DIR=./bin/linux/release/rdm.app/Contents/
+BUNDLE_PATH=./bin/linux/release/rdm.app 
+BUILD_DIR=$BUNDLE_PATH/Contents/
+MAC_TOOL=/Users/administrator/Qt5.1.1/5.1.1/clang_64/bin/macdeployqt
 
-cp -f ./redis-desktop-manager/Info.plist $BUILD_DIR/Info.plist
-cp -f ./redis-desktop-manager/rdm.icns $BUILD_DIR/Resources/rdm.icns 
+cp -f ./redis-desktop-manager/Info.plist $BUILD_DIR/
+cp -f ./redis-desktop-manager/rdm.icns $BUILD_DIR/Resources/
+
+$MAC_TOOL $BUNDLE_PATH -dmg 
