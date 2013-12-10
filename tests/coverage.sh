70bb82bd2e48ca5fee53790202f0ff0897e6550c
Igor Malinovskiy <u.glide@gmail.com>
2013-12-10 22:57:01 +0200

    Fixed coveralls config

--- tests/coverage.sh
+++ tests/coverage.sh
@@ -3,5 +3,3 @@ cp -Rf ./../redis-desktop-manager/include ./
 cp -Rf ./../redis-desktop-manager/source ./
 
 find . -name '*.cpp' -exec gcov -s ../redis-desktop-manager -o ./../bin/tests/obj {} \;
-
-coveralls --exclude-pattern '.*\.h' --verbose --no-gcov
