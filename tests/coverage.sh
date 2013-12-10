351fe433fcd4c5f8d971c0abf8370d567da5302d
Igor Malinovskiy <u.glide@gmail.com>
2013-12-10 22:25:01 +0200

    Update coverage generation

--- tests/coverage.sh
+++ tests/coverage.sh
@@ -2,6 +2,6 @@
 cp -Rf ./../redis-desktop-manager/include ./
 cp -Rf ./../redis-desktop-manager/source ./
 
-find . -name '*.cpp' -exec gcov -o ./../bin/tests/obj {} \;
+find . -name '*.cpp' -exec gcov -s ../redis-desktop-manager -o ./../bin/tests/obj {} \;
 
-coveralls --verbose --no-gcov -t mW1rgPRX8DekvC1GxbUrENH7We8G09BgH
+coveralls --exclude-pattern '.*\.h' --verbose --no-gcov
