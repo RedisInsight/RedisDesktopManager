fc4552bfb6f033ca61760067b33557980ee1b6e7
Igor Malinovskiy <u.glide@gmail.com>
2014-02-25 17:38:13 +0400

    Improved OSX build script for CI

--- build_osxbundle.sh
+++ build_osxbundle.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+QTPATH=/Users/admin/Qt5.1.1/5.1.1/clang_64/bin
+
+export PATH=$PATH:$QTDIR
 
 echo ===========================
-TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
-echo Last tag: $TAG
+TAG=$1
+echo Version: $TAG
 echo ===========================
 
 echo ‘============== Build crash reporter ================’
@@ -18,13 +21,12 @@ qmake
 make -s clean
 make -s
 
-
 echo ‘============== Create release bundle ================’
 cd ./../
 
 BUNDLE_PATH=./bin/linux/release/ 
 BUILD_DIR=$BUNDLE_PATH/rdm.app/Contents/
-MAC_TOOL=/Users/admin/Qt5.1.1/5.1.1/clang_64/bin/macdeployqt
+MAC_TOOL=$QTDIR/macdeployqt
 
 cp -f ./redis-desktop-manager/Info.plist $BUILD_DIR/
 cp -f ./redis-desktop-manager/rdm.icns $BUILD_DIR/Resources/
