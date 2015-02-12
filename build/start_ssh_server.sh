6d95e82e4f1ef213e0f6bd43380fb04f828893c0
Igor Malinovskiy <u.glide@gmail.com>
2015-02-12 16:54:04 +0200

    Update start_ssh_server.sh

--- build/start_ssh_server.sh
+++ build/start_ssh_server.sh
@@ -6,15 +6,15 @@ echo "Password: $pass"
 sudo useradd -m -p $pass $username
 echo test:test | chpasswd
 
-cp /etc/ssh/sshd_config ~/sshd_config
-bash -c "echo '' >> ~/sshd_config"
-bash -c "echo 'AllowUsers test' >> ~/sshd_config"
-sed -i "s/Port 22/Port 2222/g" ~/sshd_config
-sed -i "s/\/etc\/ssh\//~\//g" ~/sshd_config
+sudo cp /etc/ssh/sshd_config ~/sshd_config
+sudo bash -c "echo '' >> ~/sshd_config"
+sudo bash -c "echo 'AllowUsers test' >> ~/sshd_config"
+sudo sed -i "s/Port 22/Port 2222/g" ~/sshd_config
+sudo sed -i "s/\/etc\/ssh\//~\//g" ~/sshd_config
 
-ssh-keygen -t rsa -N "" -f ~/ssh_host_rsa_key
-ssh-keygen -t dsa -N "" -f ~/ssh_host_dsa_key
-ssh-keygen -t ecdsa -N "" -f ~/ssh_host_ecdsa_key
+sudo ssh-keygen -t rsa -N "" -f ~/ssh_host_rsa_key
+sudo ssh-keygen -t dsa -N "" -f ~/ssh_host_dsa_key
+sudo ssh-keygen -t ecdsa -N "" -f ~/ssh_host_ecdsa_key
 
-`which sshd` -f ~/sshd_config
+sudo `which sshd` -f ~/sshd_config
 ps aux | grep ssh
