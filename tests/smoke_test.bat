9b746c824fd45afcb05480f422d612228b76aa28
Igor Malinovskiy <u.glide@gmail.com>
2015-04-24 11:00:12 +0300

    Update smoke_test.bat

--- tests/smoke_test.bat
+++ tests/smoke_test.bat
@@ -1,7 +1,8 @@
 @echo off
+taskkill /f /im rdm.exe
 cd build/windows/installer/resources/
 START /b rdm.exe
-TIMEOUT 5
+TIMEOUT 30
 tasklist /FI "IMAGENAME eq rdm.exe" 2>NUL | find /I /N "rdm.exe">NUL
 
 if "%ERRORLEVEL%"=="0" (
