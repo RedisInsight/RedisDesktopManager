7e1b5713c7de399f87831ed4ddd45f6dd565a053
Igor Malinovskiy <u.glide@gmail.com>
2015-07-14 02:09:47 -0700

    Update build script

--- build/build_wininstaller.bat
+++ build/build_wininstaller.bat
@@ -5,14 +5,11 @@ echo Setup Build Environment
 echo ============================
 cd ./../
 set SRCDIR=%cd%
-set QTDIR=C:\\Qt\\5.4\\msvc2012_opengl\\bin\\
-set PATH=C:\Python27\;%QTDIR%;C:\msys\1.0\bin\;%PATH%
+set QTDIR=C:\\Qt\\5.5\\mingw492_32\\bin\\
+set PATH=C:\Python27\;%QTDIR%;C:\msys32\mingw32\bin\;%PATH%
 qmake -v
-if not defined DevEnvDir (
-call "C:\\Program Files\\Microsoft Visual Studio 11.0\\VC\\vcvarsall.bat"
-)
-echo %PATH%
-echo DONE
+mingw32-make -v
+g++ -v
 
 echo ============================
 echo Set version %1
@@ -27,7 +24,7 @@ echo ============================
 echo Build Crash Reporter :
 cd ./3rdparty/crashreporter
 qmake CONFIG+=release DESTDIR=%SRCDIR%/bin/windows/release
-nmake /NOLOGO /S
+mingw32-make -s
 
 if %errorlevel% neq 0 (
  echo !!! Crashreporter Compilation Error !!!
@@ -38,7 +35,7 @@ echo ============================
 echo Build Application :
 cd %SRCDIR%/src
 qmake CONFIG+=release
-nmake /NOLOGO /S
+mingw32-make -s
 
 if %errorlevel% neq 0 (
  echo !!! Compilation Error !!!
@@ -49,7 +46,7 @@ echo ============================
 echo Export debug symbols
 echo ============================
 cd %SRCDIR%/bin/windows/release
-%SRCDIR%/3rdparty/breakpad/src/tools/windows/binaries/dump_syms.exe rdm.pdb > rdm.sym
+%SRCDIR%/3rdparty/breakpad/src/tools/windows/binaries/dump_syms.exe rdm.exe > rdm.sym
 
 echo ============================
 echo Build installer
