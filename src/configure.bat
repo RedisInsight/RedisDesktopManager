5a07aa598920c787befbb0090507c88bd4ce292f
Igor Malinovskiy <u.glide@gmail.com>
2016-05-17 10:11:59 +0300

    Update configure.bat

--- src/configure.bat
+++ src/configure.bat
@@ -1,15 +1,5 @@
 @echo off
 
-if exist nuget.exe (
-    echo Nuget found
-) else (
-    echo Download nuget cmd utility
-    powershell -Command "(New-Object Net.WebClient).DownloadFile('https://nuget.org/nuget.exe', 'nuget.exe')"
-)
-
-echo Install deps with nuget
-nuget install -Version 1.2.8.6 -OutputDirectory ./../3rdparty/windows rmt_zlib
-
 echo Run qredisclient configure.bat
 cd .\..\\3rdparty\\qredisclient\\src\\
 configure.bat
