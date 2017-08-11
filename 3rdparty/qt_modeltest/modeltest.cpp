1f75c0a5f8633fe757e573e5498ddc55ee2b7471
Igor Malinovskiy <u.glide@gmail.com>
2017-08-11 11:13:59 +0300

    Fix unit tests on Qt 5.9

--- 3rdparty/qt_modeltest/modeltest.cpp
+++ 3rdparty/qt_modeltest/modeltest.cpp
@@ -444,7 +444,7 @@ void ModelTest::data()
     QVariant textAlignmentVariant = model->data ( model->index ( 0, 0 ), Qt::TextAlignmentRole );
     if ( textAlignmentVariant.isValid() ) {
         int alignment = textAlignmentVariant.toInt();
-        QCOMPARE( alignment, ( alignment & ( Qt::AlignHorizontal_Mask | Qt::AlignVertical_Mask ) ) );
+        QCOMPARE((unsigned int) alignment, ( alignment & ( Qt::AlignHorizontal_Mask | Qt::AlignVertical_Mask ) ) );
     }
 
     // General Purpose roles that should return a QColor
