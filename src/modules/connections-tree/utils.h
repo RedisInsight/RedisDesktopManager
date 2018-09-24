cc35ede09d28279422584d7a5d527c834f9add07
Igor Malinovskiy <u.glide@gmail.com>
2018-09-24 16:31:00 +0300

    Remove unused function

--- src/modules/connections-tree/utils.h
+++ src/modules/connections-tree/utils.h
@@ -8,8 +8,4 @@ namespace ConnectionsTree {
 void confirmAction(QWidget* parent, const QString& msg, std::function<void()> action,
                    QString title = "Confirm action");
 
-void confirmAction(QWidget* parent, const QString& msg, std::function<void()> action,
-                   std::function<void()> noCallback,
-                   QString title = "Confirm action");
-
 }
