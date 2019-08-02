3b64538d251bee0823a02fdb7e964a5e94ace7b1
Jack Cherng <jfcherng@gmail.com>
2019-08-02 21:53:58 +0800

    Update rdm_zh_TW.ts for 2019.3
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- src/resources/translations/rdm_zh_TW.ts
+++ src/resources/translations/rdm_zh_TW.ts
@@ -445,7 +445,7 @@
         <location filename="../../qml/value-editor/ValueTabs.qml" line="444"/>
         <location filename="../../qml/value-editor/ValueTabs.qml" line="659"/>
         <source>Add Element to HLL</source>
-        <translation type="unfinished"></translation>
+        <translation>新增元素到 HHL</translation>
     </message>
     <message>
         <location filename="../../qml/value-editor/ValueTabs.qml" line="485"/>
