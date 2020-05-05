73474f76474d089236e936b23f55e1fd5407f436
Mrn Om <ommarinaom@gmail.com>
2020-05-05 13:58:58 +0300

    Add save to file button

--- src/app/qmlutils.h
+++ src/app/qmlutils.h
@@ -27,6 +27,7 @@ public:
     Q_INVOKABLE bool fileExists(const QString& path);
 
     Q_INVOKABLE void copyToClipboard(const QString &text);
+    Q_INVOKABLE bool saveToFile(const QVariant &value, const QString &path);
     Q_INVOKABLE void addNewValueToDynamicChart(QtCharts::QXYSeries* series, qreal value);
     Q_INVOKABLE QObject* wrapLargeText(const QByteArray &text);
     Q_INVOKABLE void deleteTextWrapper(QObject* w);
