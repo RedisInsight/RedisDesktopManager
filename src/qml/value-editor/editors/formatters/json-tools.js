ca6f3bdde1c31ec869bd71323338c52ef5353775
Igor Malinovskiy <u.glide@gmail.com>
2020-05-07 14:20:19 +0300

    Replace js minifier
    Fix #4770

--- src/qml/value-editor/editors/formatters/json-tools.js
+++ src/qml/value-editor/editors/formatters/json-tools.js
@@ -93,34 +93,63 @@ var prettyPrint = function( json, style ) {
     return '<pre id="value" style="width:97%; white-space: pre-wrap;' + style + '">' + highlighted + '</pre>';
 };
 
-
 /*!
     Based on:
 
-    * pretty-data - nodejs plugin to pretty-print or minify data in XML, JSON and CSS formats.
-    *
-    * Version - 0.40.0
-    * Copyright (c) 2012 Vadim Kiryukhin
-    * vkiryukhin @ gmail.com
-    * http://www.eslinstructor.net/pretty-data/
-    * Dual licensed under the MIT and GPL licenses
-*/
-var minify =  function(json) {
+    JSON.minify()
+    v0.1 (c) Kyle Simpson
+    MIT License
 
-    return  json.replace(/\s{0,}\{\s{0,}/g,"{")
-                .replace(/\s{0,}\[$/g,"[")
-                .replace(/\[\s{0,}/g,"[")
-                .replace(/:\s{0,}\[/g,':[')
-                .replace(/\s{0,}\}\s{0,}/g,"}")
-                .replace(/\s{0,}\]\s{0,}/g,"]")
-                .replace(/\"\s{0,}\,/g,'",')
-                .replace(/\,\s{0,}\"/g,',"')
-                .replace(/\"\s{0,}:/g,'":')
-                .replace(/:\s{0,}\"/g,':"')
-                .replace(/:\s{0,}\[/g,':[')
-                .replace(/\,\s{0,}\[/g,',[')
-                .replace(/\,\s{2,}/g,', ')
-                .replace(/\]\s{0,},\s{0,}\[/g,'],[');
+    https://github.com/getify/JSON.minify/blob/javascript/minify.json.js
+*/
+var minify = function(json) {
+
+    var tokenizer = /"|(\/\*)|(\*\/)|(\/\/)|\n|\r/g,
+    in_string = false,
+    in_multiline_comment = false,
+    in_singleline_comment = false,
+    tmp, tmp2, new_str = [], ns = 0, from = 0, lc, rc;
+
+    tokenizer.lastIndex = 0;
+
+    while (tmp = tokenizer.exec(json)) {
+        lc = RegExp.leftContext;
+        rc = RegExp.rightContext;
+        if (!in_multiline_comment && !in_singleline_comment) {
+            tmp2 = lc.substring(from);
+            if (!in_string) {
+                tmp2 = tmp2.replace(/(\n|\r|\s)*/g,"");
+            }
+            new_str[ns++] = tmp2;
+        }
+        from = tokenizer.lastIndex;
+
+        if (tmp[0] == "\"" && !in_multiline_comment && !in_singleline_comment) {
+            tmp2 = lc.match(/(\\)*$/);
+            if (!in_string || !tmp2 || (tmp2[0].length % 2) == 0) {	// start of string with ", or unescaped " character found to end string
+                in_string = !in_string;
+            }
+            from--; // include " character in next catch
+            rc = json.substring(from);
+        }
+        else if (tmp[0] == "/*" && !in_string && !in_multiline_comment && !in_singleline_comment) {
+            in_multiline_comment = true;
+        }
+        else if (tmp[0] == "*/" && !in_string && in_multiline_comment && !in_singleline_comment) {
+            in_multiline_comment = false;
+        }
+        else if (tmp[0] == "//" && !in_string && !in_multiline_comment && !in_singleline_comment) {
+            in_singleline_comment = true;
+        }
+        else if ((tmp[0] == "\n" || tmp[0] == "\r") && !in_string && !in_multiline_comment && in_singleline_comment) {
+            in_singleline_comment = false;
+        }
+        else if (!in_multiline_comment && !in_singleline_comment && !(/\n|\r|\s/.test(tmp[0]))) {
+            new_str[ns++] = tmp[0];
+        }
+    }
+    new_str[ns++] = rc;
+    return new_str.join("");
 };
 
 if (typeof WorkerScript !== "undefined") {
