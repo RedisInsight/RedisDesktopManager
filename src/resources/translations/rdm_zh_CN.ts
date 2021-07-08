cd8b62e8168d48b721c5442c70d3a41ce009f743
Jerry <27206617@qq.com>
2021-07-08 10:50:45 +0800

    Update rdm_zh_CN.ts

--- src/resources/translations/rdm_zh_CN.ts
+++ src/resources/translations/rdm_zh_CN.ts
@@ -479,7 +479,7 @@
     <message>
         <location filename="../../qml/ConnectionSettignsDialog.qml" line="144"/>
         <source>How to connect</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">怎么连接</translation>
     </message>
     <message>
         <location filename="../../qml/ConnectionSettignsDialog.qml" line="151"/>
@@ -489,62 +489,62 @@
     <message>
         <location filename="../../qml/ConnectionSettignsDialog.qml" line="180"/>
         <source>Create connection from Redis URL</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">从Redis URL创建连接</translation>
     </message>
     <message>
         <location filename="../../qml/ConnectionSettignsDialog.qml" line="233"/>
         <source>Learn more about Redis URL:  </source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">认识Redis URL: </translation>
     </message>
     <message>
         <location filename="../../qml/ConnectionSettignsDialog.qml" line="241"/>
         <source>Connection guides</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">连接向导</translation>
     </message>
     <message>
         <location filename="../../qml/ConnectionSettignsDialog.qml" line="262"/>
         <source>Local or Public Redis</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">本地或对外的Redis</translation>
     </message>
     <message>
         <location filename="../../qml/ConnectionSettignsDialog.qml" line="278"/>
         <source>Redis with SSL/TLS</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">使用SSL/TLS的Redis</translation>
     </message>
     <message>
         <location filename="../../qml/ConnectionSettignsDialog.qml" line="294"/>
         <source>SSH tunnel</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">SSH通道</translation>
     </message>
     <message>
         <location filename="../../qml/ConnectionSettignsDialog.qml" line="310"/>
         <source>UNIX socket</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">UNIX套接字</translation>
     </message>
     <message>
         <location filename="../../qml/ConnectionSettignsDialog.qml" line="397"/>
         <source>Cannot figure out how to connect to your redis-server?</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">不知道怎么连接到您的Redis服务端吗？</translation>
     </message>
     <message>
         <location filename="../../qml/ConnectionSettignsDialog.qml" line="403"/>
         <source>&lt;a href=&quot;https://docs.rdm.dev/en/latest/quick-start/&quot;&gt;Read the Docs&lt;/a&gt;, &lt;a href=&quot;mailto:support@rdm.dev&quot;&gt;Contact Support&lt;/a&gt; or ask for help in our &lt;a href=&quot;https://t.me/RedisDesktopManager&quot;&gt;Telegram Group&lt;/a&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">&lt;a href=&quot;https://docs.rdm.dev/en/latest/quick-start/&quot;&gt;查看文档&lt;/a&gt;, &lt;a href=&quot;mailto:support@rdm.dev&quot;&gt;联系支持&lt;/a&gt; 或者点这里寻求帮助 &lt;a href=&quot;https://t.me/RedisDesktopManager&quot;&gt;Telegram Group&lt;/a&gt;</translation>
     </message>
     <message>
         <location filename="../../qml/ConnectionSettignsDialog.qml" line="415"/>
         <source>Don&apos;t have running Redis?</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Redis没有启动吗？</translation>
     </message>
     <message>
         <location filename="../../qml/ConnectionSettignsDialog.qml" line="421"/>
         <source>Spin up hassle-free Redis on Digital Ocean</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">快速使用Digital Ocean上的Redis</translation>
     </message>
     <message>
         <location filename="../../qml/ConnectionSettignsDialog.qml" line="433"/>
         <source>Skip</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">跳过</translation>
     </message>
     <message>
         <location filename="../../qml/ConnectionSettignsDialog.qml" line="468"/>
@@ -823,7 +823,7 @@
         <location filename="../../qml/GlobalSettings.qml" line="110"/>
         <location filename="../../qml/GlobalSettings.qml" line="124"/>
         <source>Dark Mode</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">暗色模式</translation>
     </message>
     <message>
         <location filename="../../qml/GlobalSettings.qml" line="202"/>
@@ -1878,22 +1878,22 @@
     <message>
         <location filename="../../qml/QuickStartDialog.qml" line="11"/>
         <source>Getting Started</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">使用入门</translation>
     </message>
     <message>
         <location filename="../../qml/QuickStartDialog.qml" line="41"/>
         <source>Thank you for choosing RDM. Let&apos;s make your Redis experience better.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">感谢您选择RDM，我们一起让Redis有更好的体验。</translation>
     </message>
     <message>
         <location filename="../../qml/QuickStartDialog.qml" line="59"/>
         <source>Connect to Redis-Server</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">连接到Redis服务器</translation>
     </message>
     <message>
         <location filename="../../qml/QuickStartDialog.qml" line="72"/>
         <source>Read the Docs</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">阅读文档</translation>
     </message>
 </context>
 </TS>
