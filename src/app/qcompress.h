766907cacf12c1a4beeae9725f002dbe7dd87a64
Igor Malinovskiy <u.glide@gmail.com>
2021-11-18 17:53:25 +0200

    Add support for Magento/PHP compressed sessions
    Close #5028

--- src/app/qcompress.h
+++ src/app/qcompress.h
@@ -4,7 +4,29 @@
 
 namespace qcompress {
 
-enum { UNKNOWN, GZIP, LZ4, ZSTD };
+enum {
+  UNKNOWN,
+  GZIP,
+  LZ4,
+  ZSTD,
+  /*
+   * MAGENTO session is build on top of the following php lib to store
+   *compressed sessions:
+   * https://github.com/colinmollenhour/php-redis-session-abstract/blob/5f399c53534cd1fe07460407e510590840b2c6d0/src/Cm/RedisSession/Handler.php#L822-L828
+   **/
+  MAGENTO_SESSION_GZIP,
+  MAGENTO_SESSION_LZ4,
+  // MAGENTO_SESSION_ZSTD, // ZSTD is not yet supported -
+  // https://github.com/colinmollenhour/php-redis-session-abstract/issues/42
+
+  /*
+   * MAGENTO CACHE
+   * https://github.com/colinmollenhour/Cm_Cache_Backend_Redis/blob/a9c4a5ae6001e04097aa7302abd89c9496c563e0/Cm/Cache/Backend/Redis.php#L1202-L1207
+   */
+  MAGENTO_CACHE_GZIP,
+  MAGENTO_CACHE_LZ4,
+  MAGENTO_CACHE_ZSTD
+};
 
 unsigned guessFormat(const QByteArray& val);
 
